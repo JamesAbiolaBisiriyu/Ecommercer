d7aceb416675954fa70f1e4d0830c01a088f8d48
james <emperor2g@gmail.com>
2024-01-02 21:01:40 +0100

    Description

---
+++ src/Components/descriptionBox/DescriptionBox.jsx
@@ -0,0 +1,23 @@
+import React from 'react'
+import './DescriptionBox.css'
+
+const DescriptionBox = () => {
+  return (
+    <div className='descriptionBox'>
+      <div className="descriptionBox-navigator">
+      <div className="descriptionBox-nav-box">  Description   </div>
+        <div className="descriptionBox-nav-box fade">  Reviews (122)   </div>
+      </div>
+       
+        <div className="descriptionBox-description">
+       
+          <p>
+            Lorem ipsum dolor sit, amet consectetur adipisicing elit. Eum neque commodi, deleniti quis asperiores odio sed delectus, facilis ut magni cum accusamus, placeat incidunt in consequatur dignissimos. Reiciendis ea esse eius at corrupti cum neque fugit quasi natus, quas aperiam molestiae illum assumenda quo laudantium debitis excepturi delectus deleniti, nostrum minima animi. Quas magnam culpa a accusamus, unde omnis aperiam. Non explicabo aliquam quisquam delectus fuga quo dolore recusandae, quam perspiciatis minus tempora labore quibusdam vero voluptate, possimus voluptatem tempore?
+          </p>
+          <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Porro, placeat repellendus fuga repudiandae quasi eius, ut accusantium tempore aperiam nemo suscipit labore reprehenderit deserunt voluptas consectetur. Non tempore rem deleniti iusto harum obcaecati reiciendis? Quidem nemo vero porro impedit itaque.</p>
+        </div>    
+        </div>
+  )
+}
+
+export default DescriptionBox
--- src/Pages/Product.jsx
+++ src/Pages/Product.jsx
@@ -3,6 +3,7 @@ import { ShopContext } from '../Context/ShopContext'
 import { useParams } from 'react-router-dom';
 import BreadCrums from '../Components/breadcrums/BreadCrums';
 import ProductDisplay from '../Components/productDisplay/ProductDisplay';
+import DescriptionBox from '../Components/descriptionBox/DescriptionBox';
 
 const Product = () => {
   const {all_product} = useContext(ShopContext);
@@ -15,6 +16,7 @@ const Product = () => {
     <div>
       <BreadCrums product = {product} />
       <ProductDisplay product={product} />
+      <DescriptionBox/>
     </div>
   )
 }
