da4c1aaf923448eb3b734a897d107bade43d31f4
james <emperor2g@gmail.com>
2024-01-06 11:49:07 +0100

    promobox_submit_button

--- src/Context/ShopContext.jsx
+++ src/Context/ShopContext.jsx
@@ -2,6 +2,11 @@ import React, { createContext, useState } from "react";
 import all_product from '../Components/Assets/all_product'
 
 
+
+
+
+
+
 export const ShopContext = createContext(null);
 
 // creating a function that will create an empty cart that will be the key values will be quantity of product added in the cart
@@ -19,19 +24,6 @@ const ShopContextProvider = (props)=> {
   // creating a UseState variable, cartItems is the variable name, setCartItems is the function name, pass the function getDefaultCart
   const [cartItems,setCartItems] = useState(getDefaultCart());
 
-
-
-  // creating the ADDTOCART function so we can add the object products into our cart
-  const addToCart = (itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
-    console.log(cartItems);
-  }
-
-  // creating the REMOVECART function so we can remove from the cart
-  const removeFromCart = (itemId)=>{
-    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
-  }
-
   const getTotalCartAmount = () => {
     let totalAmount = 0;
     for (const item in cartItems)
@@ -42,9 +34,23 @@ const ShopContextProvider = (props)=> {
       }
       return totalAmount;
     }
+    console.log(getTotalCartAmount);
   }
+
+  // creating the ADDTOCART function so we can add the object products into our cart
+  const addToCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]+1}));
+    // console.log(cartItems);
+  }
+
+  // creating the REMOVECART function so we can remove from the cart
+  const removeFromCart = (itemId)=>{
+    setCartItems((prev)=>({...prev,[itemId]:prev[itemId]-1}))
+  }
+
+
   
-  const contextValue = {getTotalCartAmount, all_product, cartItems,addToCart,removeFromCart};
+  const contextValue = {getTotalCartAmount,all_product, cartItems,addToCart,removeFromCart};
 
   
 
